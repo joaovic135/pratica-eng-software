4197afab478e56dd1141cd3b3ce297f3a1420bca
joaovic135 <jvba@icomp.ufam.edu.br>
2023-09-14 17:01:40 -0400

    listagem de usuarios sem  busca

--- src/components/signOut.js
+++ src/components/signOut.js
@@ -1,14 +1,31 @@
-import { signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react"
 import { Button } from "@mui/material"
 
 export default function SignOut(){
-  return ( 
-    <Button 
-      onClick={() => signOut()}
-      variant="contained"
-      sx={{ mt: 3, mb: 2 }}
-    >
-      Sign Out
-    </Button> 
-  )
+  const { data:session } = useSession()
+  if (session) {
+    return ( 
+      <Button 
+        onClick={() => signOut()}
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+      >
+        Sign Out
+      </Button> 
+    )
+
+  }else{
+    return (
+      <>
+        <Button 
+          onClick={() => signIn()}
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign in
+        </Button>
+      </>
+    )
+  }
+
 }
