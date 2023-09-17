2052da78bfd5522b4e2f133d69f0cc299c189155
joaovic135 <jvba@icomp.ufam.edu.br>
2023-09-17 18:37:23 -0400

    gerenciar produtos dos lojistas como admin

--- src/models/produto.js
+++ src/models/produto.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Produto.belongsTo(models.Lojista,{ foreignKey: 'id'})
+      Produto.belongsTo(models.Lojista,{ foreignKey: 'idLojista'})
       // define association here
     }
   }
