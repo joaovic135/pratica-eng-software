17766654e6636c8c5c9f32b21a77b0f5523432e2
joaovic135 <jvba@icomp.ufam.edu.br>
2023-09-28 17:15:07 -0400

    deploy do banco de dados

--- src/pages/api/lojista/index.js
+++ src/pages/api/lojista/index.js
@@ -36,25 +36,6 @@ export default async function handler(req, res) {
           descricao: user.descricao,
         });
         const id = novoLojista.id;
-      res.status(200).json({ name: 'erro J' });
-      break;
-
-    case 'PUT':
-      try {
-        const params = req.query;
-        await Lojista.update(req.body, { where: { id: params.id, idLojista: params.idLojista } })
-
-      } catch (e) { console.log(e) }
-      res.status(200).json({ name: 'erro J' });
-      break;
-
-    case 'DELETE':
-      try {
-        const params = req.query;
-        await Lojista.destroy({ where: { id: params.id, idLojista: params.idLojista } })
-      } catch (e) { console.log(e) }
-      res.status(200).json({ name: 'erro J' });
-      break;
 
         res.status(200).json({ name: 'erro J' });
         break;
