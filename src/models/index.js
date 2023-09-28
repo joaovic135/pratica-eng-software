3f9e7f62e17a6651ee1e364993e75a40a1f2adf2
joaovic135 <jvba@icomp.ufam.edu.br>
2023-09-28 17:15:07 -0400

    deploy do banco de dados

--- src/models/index.js
+++ src/models/index.js
@@ -4,10 +4,8 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-let modelPath = path.resolve('src', 'models'); //add this line
-console.log(modelPath)
-console.log("caminho")
+let modelPath = path.resolve(process.cwd() , 'src' , 'models'); //add this line
+
 const basename = path.basename(__dirname + '/../models/index.js'); //change this line
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
@@ -15,34 +13,28 @@ import mysql2 from 'mysql2';
 
 const db = {};
 
-const sequelize = new Sequelize('db', 'jcghtq93gd961qgtppej', 'pscale_pw_mWN466AzFLvkzP7PRXEUt0Xu7fAh3lXpfFNxn9tjRM3', {
-  host: 'aws.connect.psdb.cloud',
-  dialect: 'mysql',
-  dialectModule: mysql2,
-  dialectOptions: {
-      ssl: {
-          rejectUnauthorized: true,        
-      }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(modelPath) //change this line
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(__dirname + '/../models/' + file)(sequelize, Sequelize.DataTypes); //change this line
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
   }
 });
-
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connected to PlanetScale!');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  } 
-})(); 
-
-const Lojista = require('./lojista')(sequelize, Sequelize); // Importe o modelo Lojista
-const Usuario = require('./usuario')(sequelize, Sequelize); // Importe o modelo Lojista
-const Produto = require('./produto')(sequelize, Sequelize); // Importe o modelo Lojista
-
-db.Lojista = Lojista;
-db.Usuario = Usuario;
-db.Produto = Produto;
-
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
