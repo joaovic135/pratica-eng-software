739f30764017529f3b53d0be06a76f46ddefda1c
joaovic135 <jvba@icomp.ufam.edu.br>
2023-09-28 20:55:26 -0400

    deploy do banco de dados

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-let modelPath = path.resolve(process.cwd(), 'src', 'models'); //add this line
+let modelPath = path.resolve('./', 'src', 'models'); //add this line
 console.log(modelPath)
 console.log("caminho")
 const basename = path.basename(__dirname + '/../models/index.js'); //change this line
