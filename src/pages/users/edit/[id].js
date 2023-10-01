96ad4445db35f6875d926754823f2c2afdae9f3f
TeodorioNeto <83874983+TeodorioNeto@users.noreply.github.com>
2023-09-30 21:55:44 -0400

    Update [id].js

--- src/pages/users/edit/[id].js
+++ src/pages/users/edit/[id].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { APIURL } from '../../lib/constants';
+import { APIURL } from '@/lib/constants';
 import {
   Avatar,
   Box,
