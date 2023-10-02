2ef54b3af2a3668f1f378832ef13cb24b2143817
joaovic135 <jvba@icomp.ufam.edu.br>
2023-10-02 11:10:28 -0400

    teste

--- src/models/index.js
+++ src/models/index.js
@@ -11,7 +11,8 @@ console.log("caminho")
 const basename = path.basename(__dirname + '/../models/index.js'); //change this line
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
-import mysql2 from 'mysql2';
+const mysql2 = require('mysql2');
+
 
 const db = {};
 
