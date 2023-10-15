08653f22049b4f67f0caa546891248f963ecb005
Thiago Queiroz <thiago.queiroz@icomp.ufam.edu.br>
2023-10-15 17:00:47 -0400

    repaginada nas páginas de lojista e de admin

--- src/_nav_lojista.js
+++ src/_nav_lojista.js
@@ -19,42 +19,18 @@ import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
 const _nav = [
   {
     component: CNavTitle,
-    name: 'Admin',
-  },
-  {
-    component: CNavItem,
-    name: 'Cadastrar',
-    href: '/auth/admin/signup',
-    icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavTitle,
-    name: 'Usuários',
-  },
-  {
-    component: CNavItem,
-    name: 'Listar',
-    href: '/users/',
-    icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavTitle,
-    name: 'Lojistas',
-  },
-  {
-    component: CNavItem,
-    name: 'Listar',
-    href: '/lojistas/',
-    icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavTitle,
     name: 'Produtos',
   },
   {
     component: CNavItem,
-    name: 'Listar',
-    href: '/produtos/',
+    name: 'Listar Produtos',
+    href: '/produtos/lojista',
+    icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Cadastrar Produto',
+    href: '/produto/cadastrar',
     icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
   },
 ]
--- src/components/ButtonLojistaListarProduto.js
+++ src/components/ButtonLojistaListarProduto.js
@@ -2,17 +2,14 @@ import { Button } from "@mui/material";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
-
-export default function ButtonListarCompradores(){
+export default function ButtonLojistaListarProdutos(){
   const router = useRouter()
   const {data:session} = useSession()
-
-
 
   if(1){
     return(
       <>
-        <Button onClick={()=> router.push('/users/')} variant="contained">Listar usuários</Button>
+        <Button onClick={()=> router.push('/produtos/lojista')} variant="contained">Listar Produtos</Button>
       </>
     )
   }
