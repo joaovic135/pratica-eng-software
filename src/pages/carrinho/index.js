0bd6b67c77b79c3a051b6400b98ffa15069de264
Thiago Queiroz <thiago.queiroz@icomp.ufam.edu.br>
2023-10-19 23:38:55 -0400

    as rotas das páginas de carrinho, sobre e suporte foram criadas

---
+++ src/pages/carrinho/index.js
@@ -0,0 +1,10 @@
+export default function Sobre() {
+    return (
+        <>
+        <h2>
+        Você foi redirecionado com sucesso para a futura página "carrinho de compras", que 
+        será implementada somente na sprint 5. Por enquanto essa rota apenas existe na página do comprador.
+        </h2>
+        </>
+    )
+}
---
+++ src/pages/sobre/index.js
@@ -0,0 +1,10 @@
+export default function Sobre() {
+  return (
+    <>
+    <h2>
+      Você foi redirecionado com sucesso para a futura página "sobre", que 
+      será implementada somente na sprint 6. Por enquanto essa rota apenas existe no Footer.
+    </h2>
+    </>
+  )
+}
