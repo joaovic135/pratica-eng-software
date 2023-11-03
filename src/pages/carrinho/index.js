daed5c286871e1c6fa77baf8769910cac5766386
joaovic135 <jvba@icomp.ufam.edu.br>
2023-11-03 12:34:11 -0400

    correcoes no carrinho

--- src/pages/carrinho/index.js
+++ src/pages/carrinho/index.js
@@ -1,102 +1,160 @@
 import AppAppBar from '@/components/appAppBar';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button } from '@mui/material';
-import { Card, CardContent, Container, Grid } from '@mui/material';
+import { Box, Button, IconButton, Divider } from '@mui/material';
+import { Card, CardContent, CardActionArea, Grid } from '@mui/material';
 import { useSession } from 'next-auth/react';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { removerProduto } from '@/redux/carrinhoSlice';
-// import Produto from '@/models/produto';
 import { useRouter } from 'next/router';
-import { persistor } from '@/redux/store';
+import Typography from '@/components/typography';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Carrinho() {
-    
-    const dispatch = useDispatch();
-    const router = useRouter();
-    const carrinho = useSelector(state => state.carrinho)
-    const [sessao, setSession] = useState(null);
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const carrinho = useSelector(state => state.carrinho);
+  const [sessao, setSession] = useState(null);
+  const [frete, setFrete] = useState(0);
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      return router.push('/auth/login');
+    },
+  });
 
-    const { data: session, status } = useSession({
-        required: true,
-        onUnauthenticated() {
-          return router.push('/auth/login');
-        },
-    });
+  const randomNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-    useEffect(() => {
-        if (session) {
-            setSession(session.user.usuario);
-        }
+  useEffect(() => {
+    if (session) {
+      setSession(session.user.usuario);
     }
-    , [session])
-    
-    const handleRemove = (id) => {
-        dispatch(removerProduto({id}))
-    }
+    setFrete(randomNumberInRange(1, 20));
+  }, [session]);
 
-    console.log({carrinho})
-    return (
-        <div>
-            {session && <AppAppBar sessao={session.user.usuario} />}
-            <Grid sx={{flexFlow: 1}} container spacing={4}>
-                {/* Lista de produtos */}
-                    
-                <Grid item xs={6}>
-                    {/* Mostrar Produtos */}
-                    <Card style={{maxWidth: 400, margin: '0 auto', borderRadius: 16}}>
-                        <CardContent>
-                            <h1>Carrinho</h1>    
-                            {carrinho.carrinho.map(produto => <li className='list-group-item' key={produto.id}>
-                                <>
-                                    <Card style={{maxWidth: 400, margin: '0 auto', borderRadius: 16}}>
-                                        <Grid>
-                                            <h3>Nome {produto.nome}</h3>
-                                            <p>Valor {produto.preco}</p>
-                                            <p>Quantidade x{produto.quantidade}</p>
-                                            
-                                        </Grid>
+  const handleRemove = id => {
+    dispatch(removerProduto({ id }));
+  };
 
-                                        <Grid>
-                                            <Button varaint="contained" color='error'
-                                                onClick={() => {
-                                                    handleRemove(produto.id)
-                                                }
-                                            }>x</Button>
-                                        </Grid>
-                                    </Card>
-                                    <br />
-                                </>  
-                            </li>)} 
-                        </CardContent>
-                    </Card>
-                </Grid>
-                
-                <Grid item xs={4}>
-                    <Card style={{maxWidth: 400, margin: '0 auto', borderRadius: 16}}>
-                        {/* Mostrar valor total */}
-                        <CardContent>Total : {carrinho.total}</CardContent>
+  // Calcular o subtotal
+  const subtotal = carrinho.carrinho.reduce(
+    (acc, produto) => acc + produto.preco * produto.quantidade,
+    0
+  );
 
-                        {/* Botão de finalizar compras */}
-                        <CardContent>
-                            <Card style={{maxWidth: 400, margin: '0 auto', borderRadius: 16}}>
-                                <Button onClick={
-                                    () => {
-                                        if (carrinho.carrinho.length > 0) {
-                                            router.push(`/carrinho/pagamento`)
-                                        } else {
-                                            console.log('Carrinho vazio')
-                                        }
-                                    }
-                                
-                                } varaint="contained" color="success">Finalizar Compra</Button>
-                            </Card>
-                            </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
-        </div>
-    );
+  // Calcular o total
+  const total = subtotal + frete;
+
+  // Calcular a quantidade total de itens no carrinho
+  const quantidadeItens = carrinho.carrinho.reduce(
+    (acc, produto) => acc + produto.quantidade,
+    0
+  );
+
+  return (
+    <div>
+      {session && <AppAppBar sessao={session.user.usuario} />}
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              marginLeft: '16px',
+            }}
+          >
+            <h1 style={{ margin: '10px' }}>Carrinho de Compras</h1>
+            <p style={{ marginLeft: '10px', margin: '0' }}>
+              ({quantidadeItens} itens)
+            </p>
+          </div>
+          <Divider
+            variant="middle"
+            style={{
+              margin: '16px 0',
+              height: '4px',
+              backgroundColor: '#000000', // Cor do Divider
+            }}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Card style={{ maxWidth: 800, margin: '0 auto', borderRadius: 16 }}>
+            <CardContent>
+              {carrinho.carrinho.map(produto => (
+                <li className="list-group-item" key={produto.id}>
+                  <Card sx={{ maxWidth: 800, marginBottom: '20px' }}>
+                    <CardActionArea>
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          Nome {produto.nome}
+                        </Typography>
+                        <Typography variant="body1" color="text.secondary">
+                          Valor {produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                        </Typography>
+                        <Typography variant="body1" color="text.secondary">
+                          Quantidade x{produto.quantidade}
+                        </Typography>
+                        <Grid>
+                          <IconButton
+                            aria-label="delete"
+                            color="error"
+                            size="large"
+                            onClick={() => {
+                              handleRemove(produto.id);
+                            }}
+                          >
+                            <DeleteIcon fontSize="inherit" />
+                          </IconButton>
+                        </Grid>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </li>
+              ))}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card style={{ maxWidth: 500, margin: '0 auto', borderRadius: 16 }}>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                <Box sx={{ fontWeight: 'regular', m: 1 }}>Resumo do pedido</Box>
+              </Typography>
+              <Typography gutterBottom variant="body1" component="div">
+                <Box sx={{ fontWeight: 'regular', m: 1 }}>
+                  Subtotal : {subtotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                </Box>
+              </Typography>
+              <Typography gutterBottom variant="body1" component="div">
+                <Box sx={{ fontWeight: 'regular', m: 1 }}>
+                  Frete : {frete.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                </Box>
+              </Typography>
+              <Typography gutterBottom variant="body1" component="div">
+                <Box sx={{ fontWeight: 'bold', m: 1 }}>
+                  Total : {total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                </Box>
+              </Typography>
+              <Button
+                onClick={() => {
+                  if (carrinho.carrinho.length > 0) {
+                    router.push(`/carrinho/pagamento`);
+                  } else {
+                    console.log('Carrinho vazio');
+                  }
+                }}
+                variant="contained"
+                color="success"
+              >
+                Finalizar Compra
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Carrinho;
