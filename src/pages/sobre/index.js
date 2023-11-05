474b5fb1353103e1aad81c61590c8d2466c1c072
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2023-11-05 12:27:13 -0400

    Adição de texto e imagem de fundo

--- src/pages/sobre/index.js
+++ src/pages/sobre/index.js
@@ -1,10 +1,84 @@
+import React from 'react';
+import { Container, Typography, Paper, Box } from '@mui/material';
+import AppAppBar from '@/components/appAppBar';
+import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import BannerLayout from '@/components/bannerLayout';
+import Image from 'next/image';
+import session from 'redux-persist/lib/storage/session';
+import AppFooter from '@/components/appFooter';
 export default function Sobre() {
+  const [sessao, setSession] = useState(null);
+  
+  useEffect(() => {
+    if (session) {
+      setSession(session.user.usuario);
+
+    }
+  }, []);
+
+  const { data: session, status } = useSession({
+    required: false,
+    onUnauthenticated() {
+      return router.push('/auth/login');
+    },
+  });
+
+  
   return (
     <>
-    <h2>
-      Você foi redirecionado com sucesso para a futura página &quot;sobre&quot;, que 
-      será implementada somente na sprint 6. Por enquanto essa rota apenas existe no Footer.
-    </h2>
+      <div >
+        <AppAppBar sessao={sessao} />
+        
+        <BannerLayout sxBackground={{
+          backgroundImage: `url('/fundos_sobre.jpg')`,
+          bgcolor: 'background.paper',
+          backgroundPosition: 'center',
+        }}>
+          <Typography
+            color="inherit"
+            align="center"
+            variant="h2"
+            marked="center"
+            sx={{ mb: 4 }}
+          >
+            Sobre Nossa Empresa
+          </Typography>
+          <Typography
+            color="inherit"
+            align="center"
+            variant="h5"
+            sx={{ mb: 10 }}
+          >
+            Conheça um pouco mais sobre a nossa empresa
+          </Typography>
+          <Image
+            width={500}
+            height={500}
+            style={{ display: 'none' }}
+            src="/fundos_sobre.jpg"
+            alt="increase priority"
+          />
+          <Container align="center" maxWidth="md" style={{ marginTop: '0rem' }}>
+            <Box p={3}>
+              <Typography variant="body1" paragraph>
+                Bem-vindo à nossa empresa, onde oferecemos soluções incríveis para os nossos clientes.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Nossa missão é tornar a vida mais fácil e eficiente para todos.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Fornecemos produtos e serviços de alta qualidade e inovação constante.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Seja bem-vindo à nossa jornada!
+              </Typography>
+            </Box>
+          </Container>
+        </BannerLayout>
+
+        <AppFooter />
+      </div>
     </>
   )
 }
